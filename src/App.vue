5e8cbadd9517f53c010f6f0b8953075bc6bc83be
Huor97 <huor97ka@gmail.com>
2024-04-14 21:34:17 +0200

    chore:have intruduction in first page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
 // import HelloWorld from './components/HelloWorld.vue'
-import IntroducMe from './components/IntroduceMe.vue'
+import IntroducMe from "./components/IntroduceMe.vue";
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold bg-[#1da1f2] text-white">
-    Hello world!34
-  </h1>
-  </div>
   <IntroducMe />
   <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
--- src/components/IntroduceMe.vue
+++ src/components/IntroduceMe.vue
@@ -1,7 +1,53 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <h2>intro</h2>
+  <div class="relative ml-[10rem] mt-[270px]">
+    <div>
+      <span class="lg:leading-[1] tracking-wide">
+        <div class="text-white font-[Montserrat] lg:text-[28px] font-thin">
+          Hello
+        </div>
+        <div class="font-[Montserrat] lg:text-[28px] text-white font-thin">
+          Je m'appelle Rouh
+        </div>
+      </span>
+    </div>
+    <div class="lg:leading-[150px] tracking-wide">
+      <h1 class="font-[Oswald] lg:text-[120px] font-bold text-white">
+        FULL-STACK
+      </h1>
+      <h1 class="stroke__title font-[Oswald] lg:text-[120px] font-bold">
+        DÉVELOPPEUR
+      </h1>
+    </div>
+    <div
+      class="text-white font-[Montserrat] lg:text-[28px] tracking-wide leading-[1.5] font-thin"
+    >
+      <div>
+        <span> J'aime programmer et résoudre des problèmes </span>
+      </div>
+      <div>
+        <span>
+          <strong>Persévérant</strong> et <strong>motivé</strong>, j'aspire à
+          travailler en <strong>équipe</strong> pour améliorer
+        </span>
+      </div>
+      <div>
+        <span>
+          mes compétences en <strong>analyse</strong> et en
+          <strong>organisation</strong>, et relever de
+        </span>
+      </div>
+      <div>
+        <span>nouveaux défis.</span>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+.stroke__title {
+  -webkit-text-stroke-width: 2px;
+  -webkit-text-stroke-color: #ffffff;
+}
+</style>

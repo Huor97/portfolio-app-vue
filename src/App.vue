7d04be09882f3eee3fd804d571e264fe5d1d96d2
Huor97 <huor97ka@gmail.com>
2024-04-14 22:34:36 +0200

    have projects in second section

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
 // import HelloWorld from './components/HelloWorld.vue'
+import Project from "./components/Project.vue";
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold bg-[#1da1f2] text-white">
-    Hello world!34
-  </h1>
-  </div>
+  <Project />
   <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
---
+++ src/components/Project.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center flex-col items-center">
+    <div class="relative top-12">
+      <h1 class="lg:text-[120px] font-[Oswald] tracking-wide font-bold">
+        <span class="stroke__projets p-[0.2em]">Projets</span>
+        <span class="text-white">apressier</span>
+      </h1>
+    </div>
+
+    <div class="w-[1260px] flex justify-center relative mt-40">
+      <div class="w-[855px]">
+        <img
+          src="../assets/images/vent-de-fleur.webp"
+          alt="e-comerce vent de fleur"
+        />
+      </div>
+
+      <div class="w-[356px] ml-[30px] text-white">
+        <h2 class="text-[74px] leading-[74px] font-[Oswald] tracking-wide">
+          Vente de fleurs
+        </h2>
+        <p
+          class="text-[28px] font-[Montserrat] font-thin lg:leading-[1] pt-3 pb-2"
+        >
+          J’ai conçu et réalisé ce projet en équipe, en mode Agile/MVP. Le front
+          utilise ReactJS, HTML, CSS (Tailwind CSS) et le back est en NodeJS,
+          MongoDB et JWT/token.
+        </p>
+        <div>
+          <a href="#" class="font-[Montserrat] mr-4">Site</a>
+          <a href="#" class="font-[Montserrat]">GitHub</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="w-[1260px] flex justify-center relative mt-52">
+      <div class="w-[356px] mr-[30px] text-white">
+        <h2 class="text-[74px] leading-[74px] font-[Oswald] tracking-wide">
+          Vente de fleurs
+        </h2>
+        <p
+          class="text-[28px] font-[Montserrat] font-thin lg:leading-[1] pt-3 pb-2"
+        >
+          J’ai conçu et réalisé ce projet en équipe, en mode Agile/MVP. Le front
+          utilise ReactJS, HTML, CSS (Tailwind CSS) et le back est en NodeJS,
+          MongoDB et JWT/token.
+        </p>
+        <div>
+          <a href="#" class="font-[Montserrat] mr-4">Site</a>
+          <a href="#" class="font-[Montserrat]">GitHub</a>
+        </div>
+      </div>
+      <div class="w-[855px]">
+        <img
+          src="../assets/images/vent-de-fleur.webp"
+          alt="e-comerce vent de fleur"
+        />
+      </div>
+    </div>
+
+    <div class="w-[1260px] flex justify-center relative mt-72">
+      <div class="w-[855px]">
+        <img
+          src="../assets/images/vent-de-fleur.webp"
+          alt="e-comerce vent de fleur"
+        />
+      </div>
+
+      <div class="w-[356px] ml-[30px] text-white">
+        <h2 class="text-[74px] leading-[74px] font-[Oswald] tracking-wide">
+          Vente de fleurs
+        </h2>
+        <p
+          class="text-[28px] font-[Montserrat] font-thin lg:leading-[1] pt-3 pb-2"
+        >
+          J’ai conçu et réalisé ce projet en équipe, en mode Agile/MVP. Le front
+          utilise ReactJS, HTML, CSS (Tailwind CSS) et le back est en NodeJS,
+          MongoDB et JWT/token.
+        </p>
+        <div>
+          <a href="#" class="font-[Montserrat] mr-4">Site</a>
+          <a href="#" class="font-[Montserrat]">GitHub</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="w-[1260px] flex justify-center relative mt-72">
+      <div class="w-[356px] mr-[30px] text-white">
+        <h2 class="text-[74px] leading-[74px] font-[Oswald] tracking-wide">
+          Vente de fleurs
+        </h2>
+        <p
+          class="text-[28px] font-[Montserrat] font-thin lg:leading-[1] pt-3 pb-2"
+        >
+          J’ai conçu et réalisé ce projet en équipe, en mode Agile/MVP. Le front
+          utilise ReactJS, HTML, CSS (Tailwind CSS) et le back est en NodeJS,
+          MongoDB et JWT/token.
+        </p>
+        <div>
+          <a href="#" class="font-[Montserrat] mr-4">Site</a>
+          <a href="#" class="font-[Montserrat]">GitHub</a>
+        </div>
+      </div>
+      <div class="w-[855px]">
+        <img
+          src="../assets/images/vent-de-fleur.webp"
+          alt="e-comerce vent de fleur"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.stroke__projets {
+  -webkit-text-stroke-width: 2px;
+  -webkit-text-stroke-color: #ffffff;
+}
+</style>

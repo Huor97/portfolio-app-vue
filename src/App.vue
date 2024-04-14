6f3c76d1d6d6d27028874419cff1f5900648d12e
Huor97 <huor97ka@gmail.com>
2024-04-15 00:21:21 +0200

    chore:have competence in fourth section

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
 // import HelloWorld from './components/HelloWorld.vue'
+import ExperienceContacte from "./components/ExperienceContacte.vue";
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold bg-[#1da1f2] text-white">
-    Hello world!34
-  </h1>
-  </div>
+  <ExperienceContacte />
   <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 

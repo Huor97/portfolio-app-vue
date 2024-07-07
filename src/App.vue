be3aab47f8c9feeb2c1d0856bcf471d988d90bfc
Huor97 <huor97ka@gmail.com>
2024-07-07 16:39:48 +0200

    supprimer scrolle smoother

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@ import IntroducMe from "./components/IntroduceMe.vue";
 import Projects from "./components/Projects.vue";
 import Experiences from "./components/Experiences.vue";
 import ContacteCompetences from "./components/ContacteCompetences.vue";
-import { onMounted, onUnmounted, ref } from "vue";
 import CursorAnimation from "./components/CursorAnimation.vue";
 </script>
 
